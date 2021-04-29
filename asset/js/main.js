219435d5a9721f321a249789b0d4b37c94c4c60d
kirill492 <kirnik38@gmail.com>
2021-04-29 09:59:08 +0300

    new

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,6 +12,46 @@ var i = 0;
 //}
 function selectcolor() {
 	let color = document.getElementById("color").value;
-	console.log(color);
+	// console.log(color);
 	document.body.style.background = color
+
+	// document.getElementById("result").innerHTML = color
+}
+let open_elements = document.getElementsByClassName('open');
+let numbers = document.getElementsByClassName('knobka');
+for (var i = 0; i < open_elements.length; i++) {
+	open_elements[i].addEventListener('click', open);
+}
+for (var i = 0; i < numbers.length; i++) {
+	numbers[i].addEventListener('click', addnumber);
+}
+console.log(open_elements);
+console.log(numbers);
+
+function open() {
+let o = this.value;
+console.log(o)
+add(o);
+if (o == '=') {
+	// calc();
+}
+}
+function addnumber() {
+	let n = this.value;
+	add(n);
+}
+function add(v) {
+let input = document.getElementById('enter');
+let output = input.value;
+input.value = output + v;
+}
+
+function raznost() {
+
+}
+function umnog() {
+
+}
+function del() {
+
 }
